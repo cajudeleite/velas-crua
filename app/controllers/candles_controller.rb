f4523bb92182c8ae38cc2ea3288c5035da3213d6
Nathan Debaz <nathandebaz266@gmail.com>
2021-09-23 20:04:02 -0300

    mudei tudo pra minusculo e add candle new

--- app/controllers/candles_controller.rb
+++ app/controllers/candles_controller.rb
@@ -1,5 +1,43 @@
 class CandlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @candles = Candle.all
+  end
+
+  def show
+    @candle = Candle.find(params[:id])
+  end
+
+  def new
+    @candle = Candle.new
+  end
+
+  def create
+    @candle = Candle.new(candle_params)
+    @candle.save
+    redirect_to candle_path(@candle)
+  end
+
+  def edit
+    @candle = Candle.find(params[:id])
+  end
+
+  def update
+    @candle = Candle.find(params[:id])
+    @candle.update(candle_params)
+    redirect_to candle_path(@candle)
+  end
+
+  def destroy
+    @candle = Candle.find(params[:id])
+    @candle.destroy
+    redirect_to candles_path
+  end
+
+  private
+
+  def candle_params
+    params.require(:candle).permit(:name, :stock, :description, :price, :photo)
   end
 end
--- app/models/candle.rb
+++ app/models/candle.rb
@@ -2,4 +2,5 @@ class Candle < ApplicationRecord
   belongs_to :user
   has_many :orders
   has_many :reviews
+  has_one_attached :photo
 end
